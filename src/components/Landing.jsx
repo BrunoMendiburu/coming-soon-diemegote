cdd43ef4682677aceef473a214fe221af4dba2d5
Bruno Mendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2023-04-20 08:26:07 -0300

    landing done

---
+++ src/components/Landing.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import styles from "./landing.module.css"
+import Link from "next/link"
+
+const Landing = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.title}>
+        <h1>NBA</h1>
+      </div>
+      <div className={styles.sub}>
+        <h1>ULTRA</h1>
+      </div>
+      <div className={styles.subtle}>
+        <h1>TIMES</h1>
+      </div>
+      <div className={styles.button}>
+        <Link href="info/Second">
+          <button>NEXT</button>
+        </Link>
+      </div>
+      <div className={styles.circle}></div>
+    </main>
+  )
+}
+
+export default Landing
